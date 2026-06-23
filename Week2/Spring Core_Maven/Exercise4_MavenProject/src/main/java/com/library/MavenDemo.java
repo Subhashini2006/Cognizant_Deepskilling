64258fb6a669fc43c8a8bc5884e2cf20092072fd
Subhashini Rajagopal <subhashini2452006@gmail.com>
2026-06-23 19:44:31 +0530

    Added Week2 Spring Core Maven exercises

---
+++ Week2/Spring Core_Maven/Exercise4_MavenProject/src/main/java/com/library/MavenDemo.java
@@ -0,0 +1,9 @@
+
+package com.library;
+
+public class MavenDemo {
+
+    public static void main(String[] args) {
+        System.out.println("Library Management Maven Project Configured Successfully");
+    }
+}
